c7033eb672d462a05a68addd0232d8f17d0a08b7
Josue Gullo <josuegullo163@gmail.com>
2022-05-24 19:35:42 -0400

    Se le agrego el codigo de triángulo isósceles

--- figuras.js
+++ figuras.js
@@ -68,6 +68,23 @@ function calcularAreaTriangulo() {
     alert("El area del triangulo es: " + area);
 }
 
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
+
 // Código del círculo
 
 console.group("Círculos");
