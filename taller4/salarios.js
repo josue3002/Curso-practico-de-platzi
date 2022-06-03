84f6ad8a6a8b188faaf0bd6ea69b473ce4c1ba60
Josue Gullo <josuegullo163@gmail.com>
2022-06-03 09:13:22 -0400

    Se hizo un archivo para sacar la media de salarios en colombia (aun no terminado)

---
+++ taller4/salarios.js
@@ -0,0 +1,121 @@
+const colombia = [];
+
+colombia.push({
+    name: "Valeria",
+
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Luisa",
+
+    salary: 1200,
+});
+
+colombia.push({
+    name: "Armando",
+
+    salary: 500,
+});
+
+colombia.push({
+    name: "Andrew",
+
+    salary: 1200,
+});
+
+colombia.push({
+    name: "Katherin",
+
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Gabriel",
+
+    salary: 2000,
+});
+
+colombia.push({
+    name: "Heiber",
+
+    salary: 800,
+});
+
+colombia.push({
+    name: "Esmeralda",
+
+    salary: 1300,
+});
+
+colombia.push({
+    name: "Andreina",
+
+    salary: 1100,
+});
+
+colombia.push({
+    name: "Javier",
+
+    salary: 2100,
+});
+
+colombia.push({
+    name: "Clayde",
+
+    salary: 300,
+});
+
+colombia.push({
+    name: "Carolina",
+
+    salary: 2500,
+});
+
+colombia.push({
+    name: "Ismael",
+
+    salary: 900,
+});
+
+colombia.push({
+    name: "Maria",
+
+    salary: 1050,
+});
+
+colombia.push({
+    name: "Luis",
+
+    salary: 1250,
+});
+
+colombia.push({
+    name: "Daniela",
+
+    salary: 850,
+});
+
+colombia.push({
+    name: "Daniel",
+
+    salary: 950,
+});
+
+colombia.push({
+    name: "Johanna",
+
+    salary: 2150,
+});
+
+colombia.push({
+    name: "Walter",
+
+    salary: 1550,
+});
+
+colombia.push({
+    name: "Carlos",
+
+    salary: 750,
+});
